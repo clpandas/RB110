e96b9a6712fb2dd9ddb93d68612293814dd4f62d
Crystal Lor <crystalalor@gmail.com>
2025-01-27 14:04:41 -0700

    Remove file

--- lesson_1/additional_practice1.rb
+++
@@ -1,9 +0,0 @@
-# Turn this array into a hash where the names are the keys and the values are the positions in the array.
-flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
-
-hash = {}
-flintstones.each_with_index do |name, index|
-  hash[name] = index
-end
-
-p hash # {"Fred"=>0, "Barney"=>1, "Wilma"=>2, "Betty"=>3, "Pebbles"=>4, "BamBam"=>5}

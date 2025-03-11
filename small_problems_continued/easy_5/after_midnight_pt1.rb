d8f28d13de3e429bdbadd6371928023791641c22
Crystal Lor <crystalalor@gmail.com>
2025-03-11 00:53:26 -0700

    Revisit easy 5 problem: After Midnight pt1

--- small_problems_continued/easy_5/after_midnight_pt1.rb
+++ small_problems_continued/easy_5/after_midnight_pt1.rb
@@ -6,13 +6,18 @@ def time_of_day(minutes)
   format('%02d:%02d', hrs, mins)
 end
 
-p time_of_day(0) == "00:00"
-p time_of_day(-3) == "23:57"
-p time_of_day(35) == "00:35"
-p time_of_day(-1437) == "00:03"
-p time_of_day(3000) == "02:00"
-p time_of_day(800) == "13:20"
-p time_of_day(-4231) == "01:29"
+# def time_of_day(minutes)
+#   hrs, mins = (minutes % 1440).divmod(60)
+#   format('%02d:%02d', hrs, mins)
+# end
+
+# p time_of_day(0) == "00:00"
+# p time_of_day(-3) == "23:57"
+# p time_of_day(35) == "00:35"
+# p time_of_day(-1437) == "00:03"
+# p time_of_day(3000) == "02:00"
+# p time_of_day(800) == "13:20"
+# p time_of_day(-4231) == "01:29"
 
 =begin
 Problem
